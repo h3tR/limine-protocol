a6bf6d60764ef8fcb2774500ff87fb95c841b102
h3tR <66027579+h3tR@users.noreply.github.com>
2025-12-22 00:35:08 +0100

    Added support for Date At Boot Requests

---
+++ src/requests/date_at_boot.rs
@@ -0,0 +1,29 @@
+use crate::requests::LimineRequest;
+use core::mem::MaybeUninit;
+use crate::{impl_liminine_req, LimineReqId};
+
+#[repr(C, align(8))]
+pub struct DateAtBootRequest{
+    id: LimineReqId,
+    revision: u64,
+    resp: MaybeUninit<usize>
+}
+
+impl DateAtBootRequest {
+    pub const fn new(revision: u64) -> Self {
+        Self {
+            id: LimineReqId::new([0x502746e184c088aa, 0xfbc5ec83e6327893]),
+            revision,
+            resp: MaybeUninit::uninit()
+        }
+    }
+}
+
+impl_liminine_req!(DateAtBootRequest, DateAtBootResponse);
+
+#[repr(C, align(8))]
+#[derive(Copy, Clone)]
+pub struct DateAtBootResponse {
+    revision: u64,
+    pub timestamp: u64
+}
